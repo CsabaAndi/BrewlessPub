abc7ebd5d4427dc67a92c3350c353ad59056401e
Csaba Andi <csabaandi08@gmail.com>
2026-02-20 15:22:28 +0100

    Added initial code for popup info window

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,11 +3,13 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import About from '../components/About/MainPage'
 import BackToTop from '../components/BackToTop';
+import Popup from '../components/Popup';
 
 export default function Home() {
   return (
 
     <div id='main' className='flex flex-col max-w-screen-2xl 2xl:max-w-screen-xl 2xl:m-8 items-start overflow-hidden border border-stone-900 bg-stone-900 2xl:rounded-3xl'>
+      <Popup message="Welcome to the site! This appears on every page load." />
       <Header />
       <About />
       <Footer />
